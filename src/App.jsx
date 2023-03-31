e84c4b8105f996f8a977cb25baf45705d123feb0
mahdee18 <mahdeec10@gmail.com>
2023-04-01 00:06:44 +0600

    Added react tostify

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Card from './Components/Card/Card';
 import Header from './Components/Header/Header';
 import Cafe from './Components/Cafe/Cafe';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
@@ -9,6 +11,7 @@ const App = () => {
       <Header></Header>
       <Cafe></Cafe>
       <Card></Card>
+      <ToastContainer></ToastContainer>
     </div>
   );
 };
--- src/Components/Cafe/Cafe.jsx
+++ src/Components/Cafe/Cafe.jsx
@@ -1,26 +1,28 @@
 import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 import Card from '../Card/Card';
 
-
 const Cafe = () => {
-    const [cards, setCards] = useState([])
+  const [cards, setCards] = useState([]);
+  const [cardItem, setCardItem] = useState([]);
+  const [bookmarkedTitles, setBookmarkedTitles] = useState([]);
 
-    const [cardItem, setCardItem] = useState([])
-    
-    const [bookmarkedTitle, setBookmarkedTitle] = useState([])
+  useEffect(() => {
+    fetch('KnowladgeCafe.json')
+      .then(res => res.json())
+      .then(data => setCards(data))
+  }, []);
 
-    useEffect(() => {
-        fetch('KnowladgeCafe.json')
-            .then(res => res.json())
-            .then(data => setCards(data))
-    }, [])
-    const handleToAddBookmark = (card) => {
-        let newCard = [...cardItem, card]
-        setCardItem(newCard)
-
-        setBookmarkedTitle([...bookmarkedTitle, card.title])
-
+  const handleToAddBookmark = (card) => {
+    if (bookmarkedTitles.includes(card.title)) {
+      // Show toast message if card is already bookmarked
+      toast.error('Card Title is already bookmarked!');
+    } else {
+        // Add card to card items and title to bookmarked titles
+        setCardItem([...cardItem, card]);
+        setBookmarkedTitles([...bookmarkedTitles, card.title]);
     }
+  };
     return (
         <div className="flex flex-col lg:flex-row w-full px-4 lg:px-20">
             <div className="card-container lg:w-3/5 xl:w-2/3">
@@ -35,7 +37,7 @@ const Cafe = () => {
                 <div className="blogs rounded mt-11 p-7 bg-orange-100">
                 <h2 className='text-3xl font-bold '>Bookmarked Blogs : {cardItem.length}</h2>
                 <ul >
-                    {bookmarkedTitle.map((title) => (
+                    {bookmarkedTitles.map((title) => (
                         <li className='bg-white px-3 py-5 rounded m-5 text-xl font-sans' key={title}>{title}</li>
                     ))}
                 </ul>
